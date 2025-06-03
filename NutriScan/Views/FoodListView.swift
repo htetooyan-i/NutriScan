eb6dc210bfc2c0a16afdae9155db44547e416c1b
Htet Oo Yan i <htetooyan_code@Htets-MacBook-Pro.local>
2025-06-03 19:58:06 +0700

    Make Available Food List Searchable

--- NutriScan/Views/FoodListView.swift
+++ NutriScan/Views/FoodListView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct FoodListView: View { // show the all foods that is available for this model
     @State var foodList: [String] = []
+    @State var filteredFoodList: [String] = []
+    @State var searchText: String = ""
     var body: some View {
-        List(foodList, id: \.self) { food in
-            Text(food)
-        }
-        .navigationTitle("Available Food List")
+        NavigationStack(root: {
+            List(filteredFoodList, id: \.self) { food in
+                Text(food)
+            }
+            .navigationTitle("Available Food List")
+            .searchable(text: $searchText, prompt: "Search Foods")
+        })
         .onAppear {
             self.foodList = HelperFunctions.getAvailableFoodList()
+            self.filteredFoodList = foodList
+        }
+        
+        .onChange(of: searchText) { oldValue, newValue in
+            filterFoods()
         }
         
     }
+    
+    private func filterFoods() {
+        filteredFoodList = []
+        
+        for food in foodList {
+            if food.lowercased().contains(searchText.lowercased()) {
+                filteredFoodList.append(food)
+            }
+        }
+    }
 }
 
-#Preview {
-    FoodListView()
-}
